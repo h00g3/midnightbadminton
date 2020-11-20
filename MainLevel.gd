d1159a0777ac9f8bd81fac287f2dda77226859eb
rr <rdssdf.fd@gmx.at>
2020-11-20 15:12:09 +0100

    rename players to left right and begin fixing service function

--- MainLevel.gd
+++ MainLevel.gd
@@ -1,10 +1,20 @@
 extends Node2D
 
-onready var fanny = $PlayerF
-onready var kenny = $PlayerK
+onready var playerLeft = $PlayerF
+onready var playerRight = $PlayerK
+var collisionShapeLeft
+var collisionShapeRight
+onready var shuttle = $Shuttle
 
-onready var fannyscore = 0
-onready var kennyscore = 0
+onready var serviceOffset = Vector2(0,-100)
+
+onready var playerLeftscore = 0
+onready var playerRightscore = 0
+onready var winscore = 9
+
+func _ready():
+	collisionShapeLeft = getPlayerCollisionShape(playerLeft)
+	collisionShapeRight = getPlayerCollisionShape(playerRight)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("restart") :
@@ -12,47 +22,87 @@ func _physics_process(delta):
 
 # Abschlag funktioniert noch nicht
 func abschlag(player):
-	var shuttle = preload("res://Shuttle.tscn").instance()
+	#shuttle = preload("res://Shuttle.tscn").instance()
+	#shuttle.sleeping = true
 	
-	shuttle.sleeping = true
 	
-	if player == fanny:
-		shuttle.position = fanny.position + Vector2(50,-50)
-		print ("asdasd")
-	if player == kenny:
-		shuttle.position = fanny.position + Vector2(-50,-50)
+	var shuttleTransform = shuttle.get_transform()
+	
+#	print("playerLeft: %d, %d" % [playerLeft.position.x, playerLeft.position.y])
+#	print("playerRight: %d, %d" % [playerRight.position.x, playerRight.position.y])
+#	print("shuttle before score: %d, %d" % [shuttle.position.x, shuttle.position.y])
+	
+#	print("playerLeft: %s" % playerLeft.get_child_count())
+#	print("playerRight: %s" % playerRight.get_child_count())
+	debugPlayer("left", playerLeft)
+	debugPlayer("right", playerRight)
+	print("shuttle before score: %s" % shuttleTransform)
+	
+	if player == playerLeft:
+		service(collisionShapeRight)
+	elif player == playerRight:
+		service(collisionShapeLeft)
 	else:
 		return
 	
-func kennypoint():
-	if kennyscore <= 9:
-		kennyscore+=1
-		abschlag(fanny)
-	else:
-		win(kenny)
+	print("shuttle after score: %s" % shuttleTransform)
 
-func fannypoint():
-	if fannyscore <= 9:
-		fannyscore+=1
-		abschlag(kenny)
+func service(player):
+	var shuttleBodyId = shuttle.get_rid()
+	#PhysicServer.body_get_direct_state (shuttleBodyId)
+	#shuttle.integrate_forces(state)
+	shuttle.set_sleeping(true)
+	shuttle.set_global_position(player.position + serviceOffset)
+#	shuttle.sleeping = true
+
+func playerRightpoint():
+	playerRightscore+=1
+	if playerRightscore < winscore:
+		abschlag(playerLeft)
 	else:
-		win(fanny)
+		win(playerRight)
+
+func playerLeftpoint():
+	playerLeftscore+=1
+	if playerLeftscore < winscore:
+		abschlag(playerRight)
+	else:
+		win(playerLeft)
+
+#func score(playerscore):
+#	playerscore++
+#	if(playerscore)
 
 func win(player):
 	print (" Wins")
 	get_tree().reload_current_scene()
 	
 func show_scores():
-	$Scoreboard/Fscore.text = str(fannyscore)
-	$Scoreboard/Kscore.text = str(kennyscore)
+	$Scoreboard/Fscore.text = str(playerLeftscore)
+	$Scoreboard/Kscore.text = str(playerRightscore)
 
 func _process(delta):
 	show_scores()
 
 func _on_Rechts_body_entered(body):
-	if body.get_name() == "Shuttle":
-		fannypoint()
+	if isShuttle(body):
+		playerLeftpoint()
 
 func _on_Links_body_entered(body):
-	if body.get_name() == "Shuttle":
-		kennypoint()
+	if isShuttle(body):
+		playerRightpoint()
+
+func isShuttle(body):
+	return body.get_name() == "Shuttle"
+
+func debugPlayer(side, player):
+	for child in player.get_children():
+		if child is CollisionShape2D:
+			print((child as CollisionShape2D).transform)
+#	print("%s: %s" % [side, player.get_child_count()])
+
+func getPlayerCollisionShape(player):
+	for child in player.get_children():
+		if child is CollisionShape2D:
+			return child
+	push_error ( "no collision shape found for player " % player)
--- Shuttle.gd
+++ Shuttle.gd
@@ -5,7 +5,7 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	
-	print (motion)
+	#print (motion)
 	
 	var movecurve = get_linear_velocity().normalized()
 	self.rotation = movecurve.angle()
